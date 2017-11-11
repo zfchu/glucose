fd92fb1ca518c8716e1b1e30fa3cb2d6ea549d81
shnarazk <narazakilaboratory@gmail.com>
2017-11-11 16:01:50 +0900

    drop garbage

--- simp/Main.cc
+++ simp/Main.cc
@@ -91,15 +91,15 @@ static void SIGINT_exit(int signum) {
 int main(int argc, char** argv)
 {
     try {
-      printf("c\nc This is glucose 3.0 --  based on MiniSAT (Many thanks to MiniSAT team)\nc Simplification mode is turned on\nc\n");
+      // printf("c\nc This is glucose 3.0 --  based on MiniSAT (Many thanks to MiniSAT team)\nc Simplification mode is turned on\nc\n");
       
-      setUsageHelp("c USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
+      // setUsageHelp("c USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
         
         
 #if defined(__linux__)
         fpu_control_t oldcw, newcw;
         _FPU_GETCW(oldcw); newcw = (oldcw & ~_FPU_EXTENDED) | _FPU_DOUBLE; _FPU_SETCW(newcw);
-        printf("WARNING: for repeatability, setting FPU to use double precision\n");
+        // printf("WARNING: for repeatability, setting FPU to use double precision\n");
 #endif
         // Extra options:
         //
